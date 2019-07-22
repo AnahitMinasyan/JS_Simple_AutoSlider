c4290bc46c18c64692fe12cd8d5c2184c8523b4e
AnahitMinasyan <minasyan,2111@gmail.com>
2019-07-22 21:02:08 +0400

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+
+
 const sliderImg = document.querySelectorAll('.slider-container .slider-img');
 
 const nextImgDelay = 2000;
@@ -10,10 +12,16 @@ setInterval(nextImg, nextImgDelay);
 function nextImg() {
     sliderImg[currentImgCount].style.zIndex = -2;
     const counter = currentImgCount;
+    
+    
     setTimeout(() => {
         sliderImg[counter].style.opacity = 0;
     },1000);
+    
+    
     currentImgCount = (currentImgCount + 1) % sliderImg.length;
+    
+    
     sliderImg[currentImgCount].style.opacity = 1;
     sliderImg[currentImgCount].style.zIndex = -1;
 
