2d475a903f5ebfc8ece7eb98d6d24fd04c5725f7
Anahit <minasyan.2111@gmail.com>
2019-07-22 20:26:31 +0400

    create autoSlider

---
+++ js/slide.js
@@ -0,0 +1,20 @@
+const sliderImg = document.querySelectorAll('.slider-container .slider-img');
+
+const nextImgDelay = 2000;
+let currentImgCount = 0;
+
+sliderImg[currentImgCount].style.opacity = 1;
+
+setInterval(nextImg, nextImgDelay);
+
+function nextImg() {
+    sliderImg[currentImgCount].style.zIndex = -2;
+    const counter = currentImgCount;
+    setTimeout(() => {
+        sliderImg[counter].style.opacity = 0;
+    },1000);
+    currentImgCount = (currentImgCount + 1) % sliderImg.length;
+    sliderImg[currentImgCount].style.opacity = 1;
+    sliderImg[currentImgCount].style.zIndex = -1;
+
+};
